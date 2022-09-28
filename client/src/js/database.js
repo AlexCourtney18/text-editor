ba4ce4f13ed25c81ce4b2c895b4d02d934f67570
Alex <lex.courtney18@gmail.com>
2022-09-28 10:43:58 -0500

    add functions to index.js and install.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,36 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb to database');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('text', 'readwrite');
+
+  const store = tx.objectStore('text');
+
+  const request = store.add({ content: content });
+
+  const result = await request;
+  console.log(' - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('GET from database');
+
+  const jateDb = await openDB('jate', 1);
+
+  const tx = jateDb.transaction('text', 'readonly');
+
+  const store = tx.objectStore('text');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
